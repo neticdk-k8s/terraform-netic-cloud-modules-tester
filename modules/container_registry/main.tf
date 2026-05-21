7e3096133ec5cea14a24316c4df74a40d1725f35
Tom Bramsen <tbr@netic.dk>
2026-05-21 10:39:37 +0200

    added container registry

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -5,8 +5,8 @@ resource "ovh_cloud_project_containerregistry" "registry" {
 }
 
 resource "ovh_cloud_project_containerregistry_user" "user" {
-  service_name = ovh_cloud_project_containerregistry.registry[0].service_name
-  registry_id  = ovh_cloud_project_containerregistry.registry[0].id
+  service_name = ovh_cloud_project_containerregistry.registry.service_name
+  registry_id  = ovh_cloud_project_containerregistry.registry.id
   email        = var.registry_user_email
   login        = var.registry_user_login
 }
