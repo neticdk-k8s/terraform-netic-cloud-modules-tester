8bdbd0884ad5b9eb1301ff7488e8c300a57dcb71
Tom Bramsen <tbr@netic.dk>
2026-05-22 12:43:46 +0200

    vm variable update

--- customers/netictest/test/vm/variables.tf
+++ customers/netictest/test/vm/variables.tf
@@ -1,3 +1,19 @@
+## Common variables
+variable "ovh_project_id" {}
+variable "ovh_region"   { }
+variable "ovh_api_region" { }
+variable "ovh_project_name" { } 
+
+variable "environment" {}
+variable "name_prefix"{} 
+
+# OpenStack
+variable "OS_username" {}
+variable "OS_password" {}
+
+
+## local variables
+
 variable "VMcount" {
   type    = number
   default = 1
