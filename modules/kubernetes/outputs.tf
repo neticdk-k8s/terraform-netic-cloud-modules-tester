475c49a846b49c671c2fe22a91219e165ab84268
Tom Bramsen <tbr@netic.dk>
2026-05-29 13:22:59 +0200

    kube config

--- modules/kubernetes/outputs.tf
+++ modules/kubernetes/outputs.tf
@@ -13,3 +13,9 @@ output "node_pool_ids" {
   value       = { for k, v in ovh_cloud_project_kube_nodepool.node_pool : k => v.id }
   description = "A map of the created node pools and their respective IDs"
 }
+
+output "kubeconfig" {
+  description = "Den rå kubeconfig streng fra OVH, brugt til at logge på clusteret"
+  value       = ovh_cloud_project_kube.kube_cluster.kubeconfig
+  sensitive   = true # Skjuler koden i dine GitHub logfiler
+}
